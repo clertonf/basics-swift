16b5a40e7754add6f4b612ca371b921a22195785
clertonf <clerton.filho7@gmail.com>
2023-02-13 15:21:36 -0300

    Initial Commit

---
+++ testDrive/testDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  testDriveApp.swift
+//  testDrive
+//
+//  Created by Clerton Filho on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct testDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
